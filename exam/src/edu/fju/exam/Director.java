cfafe1fd1ac20111d33c107bb822c30835bedcdb
Alvin Cheng <pop199890@gmail.com>
2018-01-16 15:33:27 +0800

    final

---
+++ exam/src/edu/fju/exam/Director.java
@@ -0,0 +1,17 @@
+package edu.fju.exam;
+
+public class Director extends Manager {
+	
+	int bonus;
+	public Director(String name, int money){
+		super(money);
+		
+	}
+	@Override
+	public void print(){
+		
+		returnmoney = 3000;
+		System.out.println(money+"\t"+bonus);
+
+}
+}
---
+++ exam/src/edu/fju/exam/Employees.java
@@ -0,0 +1,10 @@
+package edu.fju.exam;
+
+public class Employees {
+	String name;
+	int money;
+	public Employee(String name,int money){
+		this.name=name;
+		this.money=money;
+	}
+}
