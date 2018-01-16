856b517c15343fe41c0f37d09add71f9e6f5dfc9
Alvin Cheng <pop199890@gmail.com>
2018-01-16 14:41:09 +0800

    Bubble

--- exam/src/edu/fju/exam/Bubble.java
+++ exam/src/edu/fju/exam/Bubble.java
@@ -3,8 +3,24 @@ package edu.fju.exam;
 public class Bubble {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		int number[]={42,55,67,21};
+		int m,n;
+		for(m=0;m<number.length-1;m++){
+		for(n=0;n<number.length-1;n++){
+			if(number[n]<number[n+1]){
+				int tmp = number[n];
+				number[n]=number[n+1];
+				number[n+1]=tmp;
+			}
+		}
+				}
+		for (m=0;m<number.length;m++){
+			System.out.print(number[m]+" ");
+		}
+			}
 }
+		
+		
+	
+
+
